ee28f143914683026aac8e3db5d158b14eba0464
serbrio <28647145+serbrio@users.noreply.github.com>
2023-06-17 04:35:57 +0300

    c2 w4: up

--- c2-using-python-to-interact-with-operating-system/week4/check_cron.py
+++ c2-using-python-to-interact-with-operating-system/week4/check_cron.py
@@ -3,10 +3,16 @@ import re
 import sys
 
 logfile = sys.argv[1]
+usernames = {}
 with open(logfile) as f:
     for line in f:
         if "CRON" not in line:
             continue
         pattern = r"CMD \((.+)\)$"
         result = re.search(pattern, line)
-        print(result[1])
+        if result is None:
+            continue
+        name = result[1]
+        usernames[name] = usernames.get(name, 0) + 1
+        #print(result[1])
+print(usernames)
