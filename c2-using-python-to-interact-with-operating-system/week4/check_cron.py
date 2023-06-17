5351dd40d447b106ac727e8397c68e9d5b0a3722
serbrio <28647145+serbrio@users.noreply.github.com>
2023-06-17 04:25:35 +0300

    c2 w4: up

--- c2-using-python-to-interact-with-operating-system/week4/check_cron.py
+++ c2-using-python-to-interact-with-operating-system/week4/check_cron.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-
+import re
 import sys
 
 logfile = sys.argv[1]
 with open(logfile) as f:
     for line in f:
-        print(line.strip())
+        if "CRON" not in line:
+            continue
+        pattern = r"CMD \((.+)\)$"
+        result = re.search(pattern, line)
+        print(result[1])
