f19b7ac602317730cef4989de7798168c1a5ab5d
serbrio <28647145+serbrio@users.noreply.github.com>
2023-06-24 02:27:56 +0300

    c2 w5: books_test.py up

--- c2-using-python-to-interact-with-operating-system/week5/scripts/books_test.py
+++ c2-using-python-to-interact-with-operating-system/week5/scripts/books_test.py
@@ -2,6 +2,7 @@
 
 import unittest
 from books import find_book
+from books import get_keyword
 
 class TestFindBook(unittest.TestCase):
     def test_basic(self):
@@ -10,17 +11,23 @@ class TestFindBook(unittest.TestCase):
     def test_not_found(self):
         self.assertEqual(find_book('Nosuchbookexists'), [])
 
-    def test_space(self):
-        self.assertEqual(find_book(' '), [])
-
-    def test_empty_string(self):
-        self.assertEqual(find_book(''), [])
-
     def test_invalid_type(self):
         self.assertRaises(TypeError, find_book, [])
 
-    def test_new(self):
-        pass
+    def test_none(self):
+        self.assertEqual(find_book(None), None)
+
+
+class TestGetKeyword(unittest.TestCase):
+    def test_space(self):
+        self.assertEqual(get_keyword([None, " "]), None)
+
+    def test_empty_string(self):
+        self.assertEqual(get_keyword([None, ""]), None)
+
+    def test_no_arguments(self):
+        self.assertEqaul(get_keyword(["script.py"]), None)
+
 
 if __name__ == "__main__":
     unittest.main()
