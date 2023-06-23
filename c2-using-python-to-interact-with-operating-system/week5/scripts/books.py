4c5d1742afb47da4ae2c37cf9a247279ac3b4b5f
serbrio <28647145+serbrio@users.noreply.github.com>
2023-06-24 00:13:09 +0300

    c 2 w5: fix books.py

--- c2-using-python-to-interact-with-operating-system/week5/scripts/books.py
+++ c2-using-python-to-interact-with-operating-system/week5/scripts/books.py
@@ -10,7 +10,7 @@ def populate_dictionary(filename):
     with open(filename, encoding='utf-8') as csvfile:
         lines = csv.reader(csvfile, delimiter = ',')
         for row in list(lines)[3:]:
-            book = str(row[2].lower())
+            book = str(row[2])
             available = lambda row: "available" if row[0] == "TRUE" else "not available"
             read = lambda row: "read" if row[1] == "TRUE" else "not read"
             books_dict[book] = [available(row), read(row)]
@@ -26,28 +26,32 @@ def find_book(keyword):
     books_dict = populate_dictionary("../data/Available and desirable_dtsk.csv")
     found_books = []
     for book_name in books_dict.keys():
-        if keyword in book_name:
+        if keyword.lower() in book_name.lower():
             if_available = "[" + books_dict[book_name][0] + "]"
             if_read = "[" + books_dict[book_name][1] + "]"
-            found_books.append("{:<15} {:<22} {}".format(if_available, if_read, book_name)) 
-    return '\n'.join(found_books)
+            found_books.append((if_available, if_read, book_name))
+    return found_books
 
 
-def main():
+def print_found():
+    """Print nicely found books"""
     keywords = []
     try:
         for i in range(1, len(sys.argv)):
-            keywords.append(str(sys.argv[i].lower()))
+            keywords.append(str(sys.argv[i]))
         keyword = " ".join(keywords).strip()
-        books_dict = populate_dictionary("../data/Available and desirable_dtsk.csv")
         found_books = find_book(keyword)
         if len(found_books) == 0:
             return "not found"
         else:
-            return found_books
+            repr_found = []
+            for if_available, if_read, book_name in found_books:
+                repr_book = "{:<15} {:<22} {}".format(if_available, if_read, book_name)
+                repr_found.append(repr_book)
+            return '\n'.join(repr_found)
     except IndexError:
         return "Missing keyword"
 
 
 if __name__ == "__main__":
-    print(main())
+    print(print_found())
