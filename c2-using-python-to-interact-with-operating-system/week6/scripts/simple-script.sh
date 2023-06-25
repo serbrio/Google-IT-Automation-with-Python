4b27ca5ed86fafcb99d27b242701483c4429f8ae
serbrio <28647145+serbrio@users.noreply.github.com>
2023-06-25 20:45:21 +0300

    c2 w6: bash scripts

---
+++ c2-using-python-to-interact-with-operating-system/week6/scripts/simple-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Taken args quantity: $#"
+echo "The second arg: $2"
+
+
+
+
---
+++ c2-using-python-to-interact-with-operating-system/week6/scripts/stdout_example.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+data = input("From STDIN: ")
+print("Write it to STDOUT: " + data)
+raise ValueError("Error to STDERR")
