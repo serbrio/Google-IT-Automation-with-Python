3f07892d2137b4cf5963c7174209cf46132b166f
serbrio <28647145+serbrio@users.noreply.github.com>
2023-06-28 18:40:59 +0300

    c2 w6: bash random script

--- c2-using-python-to-interact-with-operating-system/week6/scripts/ryans_bash_tutorials/random-to-100.sh
+++ c2-using-python-to-interact-with-operating-system/week6/scripts/ryans_bash_tutorials/random-to-100.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+#takes two cmd arguments (lower and upper limits)
+#and returns the random int within this limits, 
+#including lower and upper
 
-lower=$1
-upper=$2
+let lower=$1
+let upper=$2+1
+echo lower: $lower
+echo upper: $upper
+echo ------------
 
-random=$((($RANDOM+$lower)%$upper))
+#random=$(($RANDOM%$upper)) #return random from 0 to upper
+
+random=$((($RANDOM%($upper-$lower))+$lower))
 echo $random
