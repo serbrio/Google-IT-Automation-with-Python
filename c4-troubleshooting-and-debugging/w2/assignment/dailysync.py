883d4347d259f34fcf7a6da6e985d3f929027e95
serbrio <28647145+serbrio@users.noreply.github.com>
2023-10-31 07:12:40 +0100

    c4: w2: Assignment: adding solution scripts

---
+++ c4-troubleshooting-and-debugging/w2/assignment/dailysync.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import subprocess
+import os
+from multiprocessing import Pool
+
+
+def run(task):
+    print(task)
+    subprocess.call(["rsync", "-vzarh", task, dest])
+
+
+src = "/data/prod/"
+dest = "/data/prod_backup/"
+
+dir_tasks = [os.path.join(src, x) for x in os.listdir(src)]
+p = Pool(len(dir_tasks))
+p.map(run, dir_tasks)
+
