f7067b4c2728a00025f25203a08c0a3577be4d86
serbrio <28647145+serbrio@users.noreply.github.com>
2023-11-04 02:06:16 +0100

    c4: w3: Add format to logging.basicConfig

--- c4-troubleshooting-and-debugging/w3/logging_from_multiple_modules/myapp.py
+++ c4-troubleshooting-and-debugging/w3/logging_from_multiple_modules/myapp.py
@@ -5,7 +5,7 @@ import mylib
 
 
 def main():
-    logging.basicConfig(filename='myapp.log', level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s||%(levelname)s||%(module)s|| %(message)s', filename='myapp.log', level=logging.INFO)
     logging.info('Started')
     mylib.do_something()
     logging.info('Finished')
